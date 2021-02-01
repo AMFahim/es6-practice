30982b8655235fe25ee5fdfb95bed6de55f3b275
AMFahim <amfahim2019@gmail.com>
2021-02-01 17:40:45 +0600

    let vs const

---
+++ letConst.js
@@ -0,0 +1,15 @@
+//const value
+const numbers = [23, 34, 54];
+numbers[2] = 24;
+// console.log(numbers);
+
+//let value
+let name = "Abdul Mozid Fahim";
+name = "A M Fahim"
+console.log(name);
+
+let sum = 0;
+for (let i = 0; i <= 10; i++) {
+  const element = sum[i];
+  console.log(i);
+}
